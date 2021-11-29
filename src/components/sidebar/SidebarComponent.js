d81a040b71e1bf7ba2fcd21461767d419b341a8b
amitkhemchand <amitkhemchand@gmail.com>
2021-11-29 14:46:26 +0530

    sparte css

--- src/components/sidebar/SidebarComponent.js
+++ src/components/sidebar/SidebarComponent.js
@@ -1,67 +1,11 @@
 import React from 'react';
 import { Column, Row  } from 'simple-flexbox';
-import { StyleSheet, css } from 'aphrodite';
+import { css } from 'aphrodite';
 import LogoComponent from './LogoComponent';
 import MenuItemComponent from './MenuItemComponent';
+import SidebarCSS from '../../css/SidebarCSS'
 
 import { IconOverview, IconTickets, IconIdeas, IconContacts,IconAgents, IconArticles, IconSettings,IconSubscription, IconBurger } from "../../assets/icons";
-
-const styles = StyleSheet.create({
-    burgerIcon: {
-        cursor: 'pointer',
-        position: 'absolute',
-        left: 24,
-        top: 34
-    },
-    container: {
-        backgroundColor: '#363740',
-        width: 255,
-        paddingTop: 32,
-        height: 'calc(100% - 32px)'
-    },
-    containerMobile: {
-        transition: 'left 0.5s, right 0.5s',
-        position: 'absolute',
-        width: 255,
-        height: 'calc(100% - 32px)',
-        zIndex: 901
-    },
-    mainContainer: {
-        height: '100%',
-        minHeight: '100vh'
-    },
-    mainContainerMobile: {
-        position: 'absolute',
-        width: '100vw',
-        minWidth: '100%',
-        top: 0,
-        left: 0
-    },
-    menuItemList: {
-        marginTop: 52
-    },
-    outsideLayer: {
-        position: 'absolute',
-        width: '100vw',
-        minWidth: '100%',
-        height: '100%',
-        backgroundColor: 'rgba(0,0,0,.50)',
-        zIndex: 900
-    },
-    separator: {
-        borderTop: '1px solid #DFE0EB',
-        marginTop: 16,
-        marginBottom: 16,
-        opacity: 0.06
-    },
-    hide: {
-        left: -255
-    },
-    show: {
-        left: 0
-    }
-});
-
 class SidebarComponent extends React.Component {
 
     state = { expanded: false };
@@ -76,7 +20,7 @@ class SidebarComponent extends React.Component {
     toggleMenu = () => this.setState(prevState => ({ expanded: !prevState.expanded }));
 
     renderBurger = () => {
-        return <div onClick={this.toggleMenu} className={css(styles.burgerIcon)}>
+        return <div onClick={this.toggleMenu} className={css(SidebarCSS.burgerIcon)}>
             <IconBurger />
         </div>
     }
@@ -86,11 +30,11 @@ class SidebarComponent extends React.Component {
         const isMobile = this.isMobile();
         return (
             <div style={{ position: 'relative' }}>
-                <Row className={css(styles.mainContainer)} breakpoints={{ 768: css(styles.mainContainerMobile) }}>
+                <Row className={css(SidebarCSS.mainContainer)} breakpoints={{ 768: css(SidebarCSS.mainContainerMobile) }}>
                     {(isMobile && !expanded) && this.renderBurger()}
-                    <Column className={css(styles.container)} breakpoints={{ 768: css(styles.containerMobile, expanded ? styles.show : styles.hide) }}>
+                    <Column className={css(SidebarCSS.container)} breakpoints={{ 768: css(SidebarCSS.containerMobile, expanded ? SidebarCSS.show : SidebarCSS.hide) }}>
                         <LogoComponent />
-                        <Column className={css(styles.menuItemList)}>
+                        <Column className={css(SidebarCSS.menuItemList)}>
                             <MenuItemComponent
                                 title="Overview" icon={IconOverview}
                                 onClick={() => this.onItemClicked('Overview')}
@@ -117,7 +61,7 @@ class SidebarComponent extends React.Component {
                                 title="Articles" icon={IconArticles}
                                 onClick={() => this.onItemClicked('Articles')}
                                 active={this.props.selectedItem === 'Articles'} />
-                            <div className={css(styles.separator)}></div>
+                            <div className={css(SidebarCSS.separator)}></div>
                             <MenuItemComponent
                                 title="Settings" icon={IconSettings}
                                 onClick={() => this.onItemClicked('Settings')}
@@ -128,7 +72,7 @@ class SidebarComponent extends React.Component {
                                 active={this.props.selectedItem === 'Subscription'} />
                         </Column>
                     </Column>
-                    {isMobile && expanded && <div className={css(styles.outsideLayer)} onClick={this.toggleMenu}></div>}
+                    {isMobile && expanded && <div className={css(SidebarCSS.outsideLayer)} onClick={this.toggleMenu}></div>}
                 </Row>
             </div>
         );
